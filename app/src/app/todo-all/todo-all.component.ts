8923b335dec2f577eecb9f4ca69985b3be0635cc
Jovin James <jovinjames33@gmail.com>
2020-07-06 20:49:04 +0530

    base working

---
+++ app/src/app/todo-all/todo-all.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-all',
+  templateUrl: './todo-all.component.html',
+  styleUrls: ['./todo-all.component.css']
+})
+export class TodoAllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

a223c4a1e3993dce8fad575b74810768b8a7daed
Jovin James <jovinjames33@gmail.com>
2020-07-06 21:57:11 +0530

    todos basic

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,18 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoAllComponent } from './todo-all/todo-all.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { TodoAllComponent } from './todo-all/todo-all.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,TodoAllComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatCardModule,MatDatepickerModule,MatButtonModule,MatListModule,MatInputModule,MatFormFieldModule,
+    BrowserAnimationsModule,MatMomentDateModule,
+    FlexLayoutModule,
+    ReactiveFormsModule 
+    
   ],
-  providers: [],
+  providers: [{provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: true}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/src/app/todo-all/todo-all.component.ts
+++ app/src/app/todo-all/todo-all.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import {todo} from '../models/todo';
 @Component({
   selector: 'app-todo-all',
   templateUrl: './todo-all.component.html',
   styleUrls: ['./todo-all.component.css']
 })
 export class TodoAllComponent implements OnInit {
-
+newTodoForm= new FormGroup({
+title:new FormControl(),
+dueDate:new FormControl('')
+});
+  todos:todo[]=[];
   constructor() { }
 
   ngOnInit(): void {
   }
+onAddToDo(){
+console.log(this.newTodoForm.value);
+this.todos.push(this.newTodoForm.value);
+}
+onDeleteToDo(){
 
+}
 }
